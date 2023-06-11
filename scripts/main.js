be02259ba5ec61dffe8f7dbd117bc3be6313abb3
mengytheworker <126213890+mengytheworker@users.noreply.github.com>
2023-06-11 11:32:00 +0800

    Update to https

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@
 */
 
 // Fetch pcat json
-fetch(new Request('http://mengytheworker.github.io/pcat2.json'))
+fetch(new Request('https://mengytheworker.github.io/pcat2.json'))
 .then((response) => {
     if (!response.ok) {
         throw new Error(`HTTP error: ${response.status}`);
